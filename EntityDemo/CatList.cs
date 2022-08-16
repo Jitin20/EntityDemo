5046cd5b1fc8283567d4c962d3387f633615dfd7
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-16 16:29:04 +0530

    Add project files.

---
+++ EntityDemo/CatList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using EntityDemo.Models;
+using Microsoft.EntityFrameworkCore;
+namespace EntityDemo
+    
+{
+  class CatList
+    {
+        static void Main()
+        {
+            NorthwindContext context = new NorthwindContext();
+            var data = from cat in context.Categories.Include("Products") select cat;
+            foreach(var cat in data)
+            {
+                Console.WriteLine($"{cat.CategoryId}---{cat.CategoryName}---{cat.Description}");
+                foreach(var prod in cat.Products)
+                {
+                    Console.WriteLine($"{prod.ProductName}---{prod.UnitPrice}");
+                }
+                Console.WriteLine("***************");
+            }
+        }
+    }
+}
---
+++ EntityDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EntityDemo.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                NorthwindContext context = new NorthwindContext();
+            //var data = from prod in context.Products.Include("Category") select prod;
+            var data = context.Products.Include("Category").ToList();
+            foreach(var prod in data)
+            {
+                Console.WriteLine($"{prod.ProductId}---{prod.ProductName}---{prod.UnitPrice}---{prod.Category.CategoryName}");
+            }
+        }
+    }
+}
